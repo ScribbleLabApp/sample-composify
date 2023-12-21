875291b8bcb1d9998507af95230942602fe01b25
N3v1 <n3v1010@gmail.com>
2023-12-21 19:46:43 +0100

    Initial Commit

---
+++ Composify/ComposifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposifyApp.swift
+//  Composify
+//
+//  Created by Nevio Hirani on 21.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ComposifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
